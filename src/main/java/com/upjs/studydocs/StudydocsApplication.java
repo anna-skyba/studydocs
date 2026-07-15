e3cf2f83a9d9c715aa690e4cf7de58c448cd5a90
Anna Skyba <skyba.anna007@gmail.com>
2026-07-15 15:01:11 +0200

    Initial Spring Boot setup with PostgreSQL and pgvector

---
+++ src/main/java/com/upjs/studydocs/StudydocsApplication.java
@@ -0,0 +1,13 @@
+package com.upjs.studydocs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudydocsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudydocsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/upjs/studydocs/StudydocsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upjs.studydocs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudydocsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
